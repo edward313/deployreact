0f2c65b078d98e44170c6db34fe95b5838346943
Edward3103 <86288625+Edward3103@users.noreply.github.com>
2023-11-03 19:32:51 +0700

    test

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,10 +5,10 @@ const Hero = () => {
     <div className='bg-[url("https://cdn.pixabay.com/photo/2022/10/03/23/41/house-7497001__340.png")] h-96 w-full bg-cover bg-center p-20'>
       <div className="flex flex-col items-center justify-center">
         <h1 className="mb-2 text-4xl font-bold text-center">
-          Hero section with background image
+          Hero section with background image 1
         </h1>
         <p className="text-lg text-center text-white">
-          This isLorem ipsum dolor sit amet consectetur adipisicing elit.
+          Test Git hub action with github page
         </p>
         <div className="mt-4">
           <button className="px-6 py-2 text-center text-white bg-indigo-600 rounded-md shadow-md">
